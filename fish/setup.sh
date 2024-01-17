39b8055c24ef56e28bb48a97f3928b530db50d81
pnwology <88574298+pnwology-code@users.noreply.github.com>
2024-01-16 18:13:43 -0800

    Update setup.sh with starship config for fish

--- fish/setup.sh
+++ fish/setup.sh
@@ -1,2 +1,6 @@
 echo '/opt/homebrew/bin/fish' >> /etc/shells 
 chsh -s /opt/homebrew/bin/fish
+
+touch ~/.config/fish/config.fish
+echo 'fish_add_path "/opt/homebrew/bin/"' >> ~/.config/fish/config.fish 
+echo 'starship init fish | source' >> ~/.config/fish/config.fish 
